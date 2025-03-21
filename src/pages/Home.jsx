a128e0239d36bb8d7b61d5e36972f8ddb502f2c3
davidhardin <thisgoestodavid@gmail.com>
2025-03-21 15:04:56 -0600

    bue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <p className='text-red-500'>home</p>
+      <p className='text-red-500'>homeostasis</p>
     </div>
   )
 }
