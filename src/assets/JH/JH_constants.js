0aee90703d402a640d4c9831c3badd52b8c0e4cf
davidhardin <thisgoestodavid@gmail.com>
2025-05-08 12:38:57 -0600

    the pope first spoke italian and then switched to spanish

--- src/assets/JH/JH_constants.js
+++ src/assets/JH/JH_constants.js
@@ -1,6 +1,7 @@
 const jh = 'jh'
 const JH_abstract =
-  'JH lives in a house owned by a family of mafia cockroaches. She eats nothing but old hats people throw off bridges. She sleeps in three feet of coagulated blood.'
+  // 'JH lives in a house owned by a family of mafia cockroaches. She eats nothing but old hats people throw off bridges. She sleeps in three feet of coagulated blood.'
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '
 const JH_cover_style = {
   width: '400px',
   height: 'auto',
