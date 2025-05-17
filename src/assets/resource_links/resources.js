fb594e3a3abbf87ff199777ef35d625d5b6c511c
davidhardin <thisgoestodavid@gmail.com>
2025-05-16 20:31:02 -0600

    resource page beginning

---
+++ src/assets/resource_links/resources.js
@@ -0,0 +1,107 @@
+const resourcesForRenters = [
+  'https://utahhousing.org/resources/resources-for-renters/',
+  'Utah Housing Coalition - Resources for Renters',
+]
+const resourcesForHomeowners = [
+  'https://utahhousing.org/resources/resources-for-homeowners/',
+  'Utah Housing Coalition - Resources for Homeowners',
+]
+const housingConnect = [
+  'https://housingconnect.org/',
+  'Housing Connect of Salt Lake County',
+]
+const roadHome = ['https://theroadhome.org/', 'The Road Home']
+
+const housingAuthSLC = [
+  'https://www.haslcutah.org/',
+  'Housing Authority of Salt Lake City',
+]
+const thrivingPlace = [
+  'https://www.slc.gov/can/thriving-in-place/',
+  'Thriving in Place',
+]
+const utahHousingCoalition = [
+  'https://www.utahhousing.org',
+  'Utah Housing Coalition',
+]
+const uhcSLConf = [
+  'https://utahhousing.org/events-research/conference/',
+  'Utah Housing Coalition’s Salt Lake County Conference on Housing',
+]
+const utahCommunityAction = [
+  'https://utahca.org/housing/',
+  'Utah Community Action',
+  ' offers services for low-income individuals and families, including housing case management.',
+]
+const unitedWay = [
+  'http://www.uw.org/211/',
+  'United Way 2-1-1 Resources & Referral ',
+  ' is a statewide service of United Way of Salt Lake that connects people to health, human, and community service programs',
+]
+const neighborNonprofit = [
+  'https://nnhc.org/',
+  'Neighborhood Nonprofit Housing Corporation',
+  ' offers housing resources for various housing settings for diverse populations in Northern Utah.',
+]
+const assistUtah = [
+  'https://www.assistutah.org/',
+  'Assist Utah',
+  ' provides architectural design, community planning, and development assistance to nonprofit and community groups; housing repair for low-income households; and accessibility design assistance to people with disabilities.',
+]
+const habitat = [
+  'https://www.habitat.org/volunteer/near-you/home-preservation',
+  'Habitat for Humanity',
+  ' offers home repair services to homeowners so they can continue to live in safe, decent homes for years to come.',
+]
+const tripleA = [
+  'http://www.faircredit.org/',
+  'AAA Fair Credit Foundation',
+  ' is a 501(c)(3) non-profit organization providing financial counseling, financial education, debt management, and asset-building programs',
+]
+
+const disabilityLaw = [
+  'http://disabilitylawcenter.org/',
+  'Disability Law Center',
+  ' is a private non-profit organization, designated as the Protection and Advocacy agency for the state of Utah, to protect the rights of people with disabilities in Utah. DLC is the group that represents Utahns in Fair Housing Act claims.',
+]
+const nationalHousing = [
+  'http://www.preservationdatabase.org/',
+  'National Housing Preservation Database',
+  ' is a search engine for affordable housing by area, housing type, funding stream, and other characteristics of affordable housing.',
+]
+const allianceCommunity = [
+  'https://alliance-community.org/',
+  'Alliance Community Services',
+  ' provides free or low-cost resources for the Latin/Hispanic community. Their mission is to promote the well-being of culturally diverse and underserved individuals by providing services for health, safety, legal, and social growth.',
+]
+const utahLabor = [
+  'http://laborcommission.utah.gov/divisions/AntidiscriminationAndLabor/fair_housing.html',
+  'Utah Labor Commission Antidiscrimination & Labor Division’s',
+  ' Fair Housing focus is to administer and enforce Utah’s Fair Housing Act.',
+]
+const utahLegal = [
+  'http://utahlegalservices.org/',
+  'Utah Legal Services',
+  ' is a nonprofit law office committed to making equal justice a reality by providing free legal help in non-criminal cases to low-income Utahns.',
+]
+export {
+  resourcesForRenters,
+  resourcesForHomeowners,
+  housingConnect,
+  roadHome,
+  housingAuthSLC,
+  thrivingPlace,
+  utahHousingCoalition,
+  uhcSLConf,
+  utahCommunityAction,
+  unitedWay,
+  neighborNonprofit,
+  assistUtah,
+  habitat,
+  tripleA,
+  disabilityLaw,
+  nationalHousing,
+  allianceCommunity,
+  utahLabor,
+  utahLegal,
+}
--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -1,7 +1,63 @@
-import React from 'react'
+import {
+  resourcesForRenters,
+  resourcesForHomeowners,
+  housingConnect,
+  roadHome,
+  housingAuthSLC,
+  thrivingPlace,
+  utahHousingCoalition,
+  uhcSLConf,
+  utahCommunityAction,
+  unitedWay,
+  neighborNonprofit,
+  assistUtah,
+  habitat,
+  tripleA,
+  disabilityLaw,
+  nationalHousing,
+  allianceCommunity,
+  utahLabor,
+  utahLegal,
+} from '../assets/resource_links/resources'
 
 const Resources = () => {
-  return <div>Resources</div>
+  const ResourceLink = ({ linkInfo }) => {
+    return (
+      <p>
+        <a className='font-bold text-blue-400' href={linkInfo[0]}>
+          {linkInfo[1]}
+        </a>
+        {linkInfo[2] ? <span className='text-xs'>{linkInfo[2]}</span> : <></>}
+      </p>
+    )
+  }
+
+  return (
+    <div className='ml-5'>
+      {/* <span className='text-2xl font-bold'>Resources</span> */}
+      <div className='flex flex-col gap-y-4 mt-2 pb-2 w-1/2'>
+        <ResourceLink linkInfo={resourcesForRenters} />
+        <ResourceLink linkInfo={resourcesForHomeowners} />
+        <ResourceLink linkInfo={housingConnect} />
+        <ResourceLink linkInfo={roadHome} />
+        <ResourceLink linkInfo={housingAuthSLC} />
+        <ResourceLink linkInfo={thrivingPlace} />
+        <ResourceLink linkInfo={utahHousingCoalition} />
+        <ResourceLink linkInfo={uhcSLConf} />
+        <ResourceLink linkInfo={utahCommunityAction} />
+        <ResourceLink linkInfo={unitedWay} />
+        <ResourceLink linkInfo={neighborNonprofit} />
+        <ResourceLink linkInfo={assistUtah} />
+        <ResourceLink linkInfo={habitat} />
+        <ResourceLink linkInfo={tripleA} />
+        <ResourceLink linkInfo={disabilityLaw} />
+        <ResourceLink linkInfo={nationalHousing} />
+        <ResourceLink linkInfo={allianceCommunity} />
+        <ResourceLink linkInfo={utahLabor} />
+        <ResourceLink linkInfo={utahLegal} />
+      </div>
+    </div>
+  )
 }
 
 export default Resources
