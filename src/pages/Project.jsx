e1c48a5e7654a03d55acc23a73b139cd0aef4781
davidhardin <thisgoestodavid@gmail.com>
2025-05-18 16:40:44 -0600

    sagan add

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,16 +1,16 @@
 import {
   kateBiography,
   jessBiography,
-  chrisBiography,
+  saganBiography,
   projectDescription,
 } from '../assets/project/biographies'
 import katePicture from '../assets/project/kate.jpg'
 import jessPicture from '../assets/project/jess_d.png'
-import chrisPicture from '../assets/project/chris.jpg'
+import saganPicture from '../assets/project/sagan.jpg'
 
 const Project = () => {
   // const style = { paddingTop: '1px' }
-  const style2 = { height: '256px' }
+  const style2 = { height: '200px', width: 'auto' }
   // const fade = 'abstract-fade'
 
   const BioTile = ({ picture, biography, style, fade }) => {
@@ -37,64 +37,57 @@ const Project = () => {
   }
 
   return (
+    // <div className=''>
     <div className=''>
-      {/* <div className='ml-4 mt-8 pb-4 w-screen overflow-x-hiddn bg-white'> */}
-      <div className=''>
-        {/* <div className='flex -ml- flex-col bg-white'> */}
-        <div className='w-full flex'>
-          <div className=''>
-            {/* <div className='flex justify-center w-screen shortest-abstract-fade'> */}
-            <div className='flex flex-row'>
-              {/* <span className='text-2xl font-bold absolute top-32 -left-4.5 rotate-270 text-nowrap md:translate-x-40'>
+      <div className='w-full flex'>
+        <div className=''>
+          <div className='flex flex-row'>
+            {/* <span className='text-2xl font-bold absolute top-32 -left-4.5 rotate-270 text-nowrap md:translate-x-40'>
                 The Project
               </span> */}
-              <div className='flex justify-end'>
-                <p className='flex flex-row w-5/6 justify-end mt-4 mr-0.5 md:mb-3'>
-                  {/* <p className='-ml-40 md:-ml-30 md:mb-4 w-[85vw] md:w-[72vw]'> */}
-                  {projectDescription}
-                </p>
-              </div>
+            <div className='flex justify-end'>
+              <p className='flex flex-row w-5/6 justify-end mt-4 mr-0.5 md:mb-3'>
+                {projectDescription}
+              </p>
             </div>
-            <span className='text-4xl md:hidden flex flex-row justify-end'>
-              {/* <span className=' text-4xl md:hidden flex flex-row'> */}
-              Team <span className='pt-0.5'>&#8627;</span>
-            </span>
           </div>
+          <span className='text-4xl md:hidden flex flex-row justify-end'>
+            Team <span className='pt-0.5'>&#8627;</span>
+          </span>
         </div>
+      </div>
 
-        <div className='flex justify-end '>
-          {/* <div className='w-screen flex justify-end md:-ml-48 '> */}
-          <div className='flex flex-row bg-blue-400 abstract-fade overflow-x-auto overflow-y-hidden no-scrollbar md:justify-en md:w-[87vw]'>
-            {/* <div className='flex flex-row justify-evenly gap-x- w-[90vw] md:w-[82.5vw] md:pl-5 bg-blue-400 abstract-fade overflow-x-auto overflow-y-hidden no-scrollbar'> */}
-            <div className='flex md:ml-44 md:justify-evenly md:gap-x-44'>
-              <div className='mt-6'>
-                <BioTile
-                  picture={katePicture}
-                  biography={kateBiography}
-                  fade={'short-abstract-fade'}
-                />
-              </div>
-              <div className='mt-6'>
-                <BioTile
-                  picture={jessPicture}
-                  biography={jessBiography}
-                  // style={style}
-                  fade={'abstract-fade'}
-                />
-              </div>
-              <div className='mt-6'>
-                <BioTile
-                  picture={chrisPicture}
-                  biography={chrisBiography}
-                  style={style2}
-                  fade={'abstract-fade'}
-                />
-              </div>
+      <div className='flex justify-end '>
+        <div className='flex flex-row bg-blue-400 abstract-fade overflow-x-auto overflow-y-hidden no-scrollbar md:justify-en md:w-[87vw]'>
+          <div className='flex md:ml-44 md:justify-evenly md:gap-x-44'>
+            <div className='mt-6'>
+              <BioTile
+                picture={katePicture}
+                biography={kateBiography}
+                style={style2}
+                fade={'short-abstract-fade'}
+              />
             </div>
-            {/* <div className='w-32'>.</div> */}
+            <div className='mt-6'>
+              <BioTile
+                picture={jessPicture}
+                biography={jessBiography}
+                style={style2}
+                fade={'abstract-fade'}
+              />
+            </div>
+            <div className='mt-6'>
+              <BioTile
+                picture={saganPicture}
+                biography={saganBiography}
+                style={style2}
+                fade={'abstract-fade'}
+              />
+            </div>
           </div>
         </div>
       </div>
+      {/* </div> */}
     </div>
   )
 }
