71658e0195c9938718f65f93607c0e1e5e798209
davidhardin <thisgoestodavid@gmail.com>
2025-05-18 15:53:02 -0600

    full stops

--- src/pages/folder_stories/B.jsx
+++ src/pages/folder_stories/B.jsx
@@ -3,7 +3,7 @@ import B_talking from '../../assets/B/B_talking.mp3'
 
 import christmas_tree from '../../assets/B/christmas_tree.JPG'
 import couch_tree from '../../assets/B/couch_tree.JPG'
-import microwave_oven from '../../assets/B/microwave_oven.jpg'
+import microwave_oven from '../../assets/B/microwave_oven.JPG'
 import night_cars from '../../assets/B/night_cars.JPG'
 import night_stair from '../../assets/B/night_stair.JPG'
 
