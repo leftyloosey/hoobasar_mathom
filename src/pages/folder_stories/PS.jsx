fbbbc09a129bf55e3fff0d12a216244a69bd2807
davidhardin <thisgoestodavid@gmail.com>
2025-05-18 15:51:57 -0600

    full stops

--- src/pages/folder_stories/PS.jsx
+++ src/pages/folder_stories/PS.jsx
@@ -4,7 +4,7 @@ import PS_talking from '../../assets/PS/PS_talking.mp3'
 import back_door from '../../assets/PS/back_door.JPG'
 import blue_front_door from '../../assets/PS/blue_front_door.JPG'
 import sky_blue_front from '../../assets/PS/sky_blue_front.JPG'
-import shower from '../../assets/PS/shower.jpg'
+import shower from '../../assets/PS/shower.JPG'
 
 import StoryPage from '../../components/StoryPage'
 
